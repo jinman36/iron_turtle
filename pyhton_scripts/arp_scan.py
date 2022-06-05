ed4a5593a99d15fdec402b4f0bc3e52ba6009748
jinman36 <jeffery.inman@hotmail.com>
2022-06-05 15:00:43 -0700

    added several working pythong scripted tools for pentesting

---
+++ pyhton_scripts/arp_scan.py
@@ -0,0 +1,12 @@
+from scapy.all import *
+
+interface = "eth0"
+ip_range = "10.6.29.23"
+broadcastMac = "ff:ff:ff:ff:ff:ff"
+
+packet = Ether(dst=broadcastMac)/ARP(pdst = ip_range) 
+
+ans, unans = srp(packet, timeout =2, iface=interface, inter=0.1)
+
+for send,receive in ans:
+        print (receive.sprintf(r"%Ether.src% - %ARP.psrc%")) 
